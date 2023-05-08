8b506ddaf1efd49d9973b102751a30befdcd2421
Ola <hollabodey.kilaso@gmail.com>
2023-05-08 06:30:35 +0100

    Changes made to the files

---
+++ script.js
@@ -0,0 +1,23 @@
+let yearResult = document.querySelector('.yearResult');
+let monthsResult = document.querySelector('.monthResult');
+let dayResult = document.querySelector('.daysResult');
+
+
+let daysInput = document.getElementById('days');
+let monthInput = document.getElementById('month');
+let yearInput = document.getElementById('year');
+
+
+
+
+daysInput.addEventListener('input', () => {
+    dayResult.innerText = daysInput.value;
+});
+
+monthInput.addEventListener('input', () => {
+    monthsResult.innerText = monthInput.value;
+});
+
+yearInput.addEventListener('input', () => {
+    yearResult.innerText = yearInput.value;
+});
