cfaf8b4a9ccfc850f7d09bd81c9ab1c278749014
Ola <hollabodey.kilaso@gmail.com>
2023-05-09 17:04:53 +0100

    Made changes to the js file

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let yearResult = document.querySelector('.yearResult');
 let monthsResult = document.querySelector('.monthResult');
 let dayResult = document.querySelector('.daysResult');
+let button = document.querySelector('.label');
 
 
 let daysInput = document.getElementById('days');
 let monthInput = document.getElementById('month');
 let yearInput = document.getElementById('year');
-
-
 
 
 daysInput.addEventListener('input', () => {
